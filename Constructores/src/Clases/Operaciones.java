4f88f902e1f1e383b3c9a088760a62c555a35bf1
Luis Ramos <ljramos6@uth.hn>
2025-10-28 11:38:23 -0600

    Refactor resultados method and handle division by zero
    Updated the resultados method to improve output formatting and added a check for division by zero.

--- Constructores/src/Clases/Operaciones.java
+++ Constructores/src/Clases/Operaciones.java
@@ -1,5 +1,5 @@
 package Clases;
-import java.util.Scanner;
+
 
 public class Operaciones {
 
@@ -11,11 +11,16 @@ public class Operaciones {
         numero1=n1;
         numero2=n2;
     }
-    public void resultados(){
-        System.out.println("La suma de "+numero1+"+"+numero2+" es: "+(numero1+numero2));
-        System.out.println("La resta de "+numero1+"-"+numero2+" es: "+(numero1-numero2));
-        System.out.println("La multiplicacion de "+numero1+"x"+numero2+" es: "+(numero1*numero2));
-        System.out.println("La division de "+numero1+"/"+numero2+" es: "+(numero1/numero2));
+    public void resultados() {
+        System.out.println("La suma de " + numero1 + "+" + numero2 + " es: " + (numero1 + numero2));
+        System.out.println("La resta de " + numero1 + "-" + numero2 + " es: " + (numero1 - numero2));
+        System.out.println("La multiplicacion de " + numero1 + "x" + numero2 + " es: " + (numero1 * numero2));
+        if (numero2 == 0) {
+            System.out.println("Error! No se puede dividir entre cero");
+        } else
+        {
+            System.out.println("La division de "+numero1+"/"+numero2+" es: "+(numero1/numero2));
+        }
 
     }
 }
